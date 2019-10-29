41b7ac3f136803c1c77967e233e96279e276f149
Mills LeMaire <jmills.lemaire@utexas.edu>
2019-10-28 19:46:08 -0500

    2D Plat movement added

--- src/scenes/TestRoom.js
+++ src/scenes/TestRoom.js
@@ -28,15 +28,37 @@ export default class TestRoom extends Phaser.Scene {
     //Add background color
     this.cameras.main.setBackgroundColor(0xEEEEEE);
     // Add walls
-    this.matter.add.rectangle(this.centerX, 0, 800, 50, {isStatic: true});
-    this.matter.add.rectangle(this.centerX, 600, 800, 50, {isStatic: true});
-    this.matter.add.rectangle(0, this.centerY, 50, 600, {isStatic: true});
-    this.matter.add.rectangle(800, this.centerY, 50, 600, {isStatic: true});
+    var ceiling = this.matter.add.rectangle(this.centerX, 0, 800, 50, {isStatic: true});
+    var floor = this.matter.add.rectangle(this.centerX, 600, 800, 50, {isStatic: true});
+    var wallL = this.matter.add.rectangle(0, this.centerY, 50, 600, {isStatic: true});
+    var wallR = this.matter.add.rectangle(800, this.centerY, 50, 600, {isStatic: true});
     // Add player sprite
     this.player = this.matter.add.sprite(this.centerX, this.centerY, 'bluebox', 50, 50, false);
+    this.player.setScale(0.5);
+    //Add keys
+    this.keys = this.input.keyboard.addKeys('w,a,s,d');
+
   }
 
   update (time, delta) {
     // Update the scene
+
+    //Keep the player upright
+    this.player.angle = 0;
+
+    //Movement
+    //Jump
+    this.keys.w.on('down', function(){
+      this.player.setVelocityY(-10)
+    }, this);
+    //Lateral Movement
+    if(this.keys.a.isDown){//Moving Left
+      this.player.setVelocityX(-4);
+    }else if(this.keys.d.isDown){//Moving right
+      this.player.setVelocityX(4);
+    }else{
+      this.player.setVelocityX(0);
+    }
+
   }
 }
